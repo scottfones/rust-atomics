d55e8f042e593db22880caa2ec6db7808812e9f1
Scott Fones <scott.fones@gmail.com>
2023-01-08 18:18:00 -0500

    ch1 done

--- chapter-1/src/main.rs
+++ chapter-1/src/main.rs
@@ -1,3 +1,35 @@
+use std::collections::VecDeque;
+use std::sync::{Condvar, Mutex};
+use std::thread;
+use std::time::Duration;
+
 fn main() {
-    println!("Hello, world!");
+    let queue = Mutex::new(VecDeque::new());
+    let not_empty = Condvar::new();
+
+    thread::scope(|s| {
+        // consuming thread
+        s.spawn(|| {
+            loop {
+                let mut q = queue.lock().unwrap();
+                let item = loop {
+                    if let Some(item) = q.pop_front() {
+                        break item;
+                    } else {
+                        q = not_empty.wait(q).unwrap();
+                    }
+
+                };
+                drop(q);
+                dbg!(item);
+            }
+        });
+
+        // producing thread
+        for i in 0.. {
+            queue.lock().unwrap().push_back(i);
+            not_empty.notify_one();
+            thread::sleep(Duration::from_secs(1));
+        }
+    });
 }
